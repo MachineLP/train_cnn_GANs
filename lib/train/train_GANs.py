655133a01593884293ab5d018cdc7fee77146bce
MachineLP <MachineLP@163.com>
2018-05-18 09:31:10 +0800

    Update train_GANs.py

--- lib/train/train_GANs.py
+++ lib/train/train_GANs.py
@@ -1,3 +1,4 @@
+
 # -*- coding: utf-8 -*-
 """
 Created on 2017 10.17
@@ -9,7 +10,7 @@ blog: http://blog.csdn.net/u014365862/article/details/78422372
 import numpy as np
 import tensorflow as tf
 slim = tf.contrib.slim
-from lib.utils.GANs_utils import build_generator, build_discriminator, cost, train_op, get_next_batch_from_path, gan_loss, gan_loss_no_log, preprocess_img
+from lib.utils.GANs_utils import build_generator, build_discriminator, cost, train_op, get_next_batch_from_path, gan_loss, preprocess_img
 from lib.utils.GANs_utils import sample_noise, generator_input_placeholder, discriminator_input_placeholder,shuffle_train_data
 import os
 import sys
@@ -17,7 +18,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 
 from tensorflow.examples.tutorials.mnist import input_data
-mnist = input_data.read_data_sets('./datasets/MNIST_data', one_hot=False)
+# mnist = input_data.read_data_sets('./datasets/MNIST_data', one_hot=False)
 
 # %matplotlib inline
 plt.rcParams['figure.figsize'] = (10.0, 8.0) # set default size of plots
@@ -45,6 +46,17 @@ def show_images(images):
         plt.imshow(img.reshape([32,32,3]))
     return 
 
+# GPU 参数配置
+def GPU_config(rate=0.8):
+    
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"      # 按照PCI_BUS_ID顺序从0开始排列GPU设备
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"    # 设置当前使用的GPU设备仅为0号设备
+    gpuConfig = tf.ConfigProto()
+    gpuConfig.allow_soft_placement = False      #设置为True，当GPU不存在或者程序中出现GPU不能运行的代码时，自动切换到CPU运行
+    gpuConfig.gpu_options.allow_growth = True       #设置为True，程序运行时，会根据程序所需GPU显存情况，分配最小的资源
+    gpuConfig.gpu_options.per_process_gpu_memory_fraction = rate    #程序运行的时，所需的GPU显存资源最大不允许超过rate的设定值
+    
+    return gpuConfig
 
 def train_GANs(train_data,train_label,valid_data,valid_label,train_dir,num_classes,batch_size,arch_model,learning_r_decay,learning_rate_base,decay_rate,dropout_prob,epoch,height,width,checkpoint_exclude_scopes,early_stop,EARLY_STOP_PATIENCE,fine_tune,train_all_layers,checkpoint_path,train_n,valid_n,g_parameter,dim = 64):
     # ---------------------------------------------------------------------------------#
@@ -71,10 +83,11 @@ def train_GANs(train_data,train_label,valid_data,valid_label,train_dir,num_class
             logits_fake, _ = build_discriminator([G_net,G_Y] , num_classes, D_keep_prob_fc, D_is_train,arch_model)
     else:
         print ('{} is error!', arch_model)
-    
-    G_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'Generator_dcgan')
+
+    #  tf.GraphKeys.TRAINABLE_VARIABLES, tf.GraphKeys.GLOBAL_VARIABLES
+    G_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'Generator_dcgan')
     # print (G_vars)
-    D_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 'Discriminator_dcgan')
+    D_vars = tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 'Discriminator_dcgan')
     # print (G_vars)
 
     D_loss, G_loss = gan_loss(logits_real, logits_fake)
@@ -96,12 +109,12 @@ def train_GANs(train_data,train_label,valid_data,valid_label,train_dir,num_class
         D_optimizer = train_op(learning_rate*0.1, D_loss, D_vars, global_step)
     elif arch_model == "arch_dcgan_conditional":
         G_optimizer = train_op(learning_rate, G_loss, G_vars, global_step)
-        D_optimizer = train_op(learning_rate*0.01, D_loss, D_vars, global_step)
+        D_optimizer = train_op(learning_rate*0.1, D_loss, D_vars, global_step)
     else:
         print ('{} is error!', arch_model)
 
     #------------------------------------------------------------------------------------#
-    sess = tf.Session()
+    sess = tf.Session(config=GPU_config())
     init = tf.global_variables_initializer()
     sess.run(init)
     saver2 = tf.train.Saver(G_vars)
@@ -131,10 +144,10 @@ def train_GANs(train_data,train_label,valid_data,valid_label,train_dir,num_class
             images, labels = get_next_batch_from_path(train_data, train_label, batch_i, height, width, batch_size=batch_size, training=True)
             if arch_model == "arch_dcgan_unconditional":
                 D_los, _ = sess.run([D_loss,D_optimizer], feed_dict={G_X: generator_x, G_is_train:True, G_keep_prob_fc:dropout_prob,D_X: images,D_is_train:True, D_keep_prob_fc:dropout_prob})
-                G_los, _ = sess.run([G_loss,G_optimizer], feed_dict={G_X: generator_x,G_is_train:True, G_keep_prob_fc:dropout_prob})
+                G_los, _ = sess.run([G_loss,G_optimizer], feed_dict={G_X: generator_x,G_is_train:True, G_keep_prob_fc:dropout_prob,D_X: images,D_is_train:True, D_keep_prob_fc:dropout_prob})
             elif arch_model == "arch_dcgan_conditional":
                 D_los, _ = sess.run([D_loss,D_optimizer], feed_dict={G_X: generator_x,G_Y:labels,G_is_train:True,G_keep_prob_fc:dropout_prob,D_X: images,D_Y:labels,D_is_train:True, D_keep_prob_fc:dropout_prob})
-                G_los, _ = sess.run([G_loss,G_optimizer], feed_dict={G_X: generator_x,G_Y:labels,G_is_train:True, G_keep_prob_fc:dropout_prob})
+                G_los, _ = sess.run([G_loss,G_optimizer], feed_dict={G_X: generator_x,G_Y:labels,G_is_train:True, G_keep_prob_fc:dropout_prob,D_X: images,D_is_train:True, D_keep_prob_fc:dropout_prob})
             else:
                 print ('{} is error!', arch_model)
             print ('D_los:', D_los)
@@ -143,11 +156,11 @@ def train_GANs(train_data,train_label,valid_data,valid_label,train_dir,num_class
             saver2.save(sess, checkpoint_path, global_step=batch_i, write_meta_graph=False)
             if batch_i%20==0:
                 if arch_model == "arch_dcgan_unconditional":
-                    D_loss_ = sess.run(D_loss, feed_dict={G_X: generator_x, G_is_train:True, G_keep_prob_fc:1.0,D_X: images,  D_is_train:True, D_keep_prob_fc:1.0})
-                    G_loss_ = sess.run(G_loss, feed_dict={G_X: generator_x,  G_is_train:True, G_keep_prob_fc:1.0})
+                    D_loss_ = sess.run(D_loss, feed_dict={G_X: generator_x, G_is_train:True, G_keep_prob_fc:1.0,D_X: images,  D_X: images,D_is_train:False, D_keep_prob_fc:1.0})
+                    G_loss_ = sess.run(G_loss, feed_dict={G_X: generator_x,  G_is_train:True, G_keep_prob_fc:1.0, D_X: images,D_is_train:False, D_keep_prob_fc:1.0})
                 elif arch_model == "arch_dcgan_conditional":
-                    D_loss_ = sess.run(D_loss, feed_dict={G_X: generator_x,G_Y:labels,G_is_train:True,G_keep_prob_fc:1.0,D_X: images,D_Y:labels,D_is_train:True, D_keep_prob_fc:1.0})
-                    G_loss_ = sess.run(G_loss, feed_dict={G_X: generator_x,G_Y:labels,G_is_train:True, G_keep_prob_fc:1.0})
+                    D_loss_ = sess.run(D_loss, feed_dict={G_X: generator_x,G_Y:labels,G_is_train:True,G_keep_prob_fc:1.0,D_X: images,D_Y:labels,D_is_train:False, D_keep_prob_fc:1.0})
+                    G_loss_ = sess.run(G_loss, feed_dict={G_X: generator_x,G_Y:labels,G_is_train:True, G_keep_prob_fc:1.0, D_X: images,D_is_train:False, D_keep_prob_fc:1.0})
                 else:
                     print ('{} is error!', arch_model)
 
@@ -161,10 +174,10 @@ def train_GANs(train_data,train_label,valid_data,valid_label,train_dir,num_class
                 images, labels = get_next_batch_from_path(valid_data, valid_label, batch_i%(int(valid_n/batch_size)), height, width, batch_size=batch_size, training=False)
                 if arch_model == "arch_dcgan_unconditional":
                     D_ls = sess.run(D_loss, feed_dict={G_X: generator_x, G_is_train:False, G_keep_prob_fc:1.0, D_X: images, D_is_train:False, D_keep_prob_fc:1.0})
-                    G_ls = sess.run(G_loss, feed_dict={G_X: generator_x, G_is_train:False, G_keep_prob_fc:1.0})
+                    G_ls = sess.run(G_loss, feed_dict={G_X: generator_x, G_is_train:False, G_keep_prob_fc:1.0, D_X: images,D_is_train:False, D_keep_prob_fc:1.0})
                 elif arch_model == "arch_dcgan_conditional":
                     D_ls = sess.run(D_loss, feed_dict={G_X: generator_x,G_Y:labels,G_is_train:False,G_keep_prob_fc:1.0,D_X: images,D_Y:labels,D_is_train:False, D_keep_prob_fc:1.0})
-                    G_ls = sess.run(G_loss, feed_dict={G_X: generator_x,G_Y:labels,G_is_train:False, G_keep_prob_fc:1.0})
+                    G_ls = sess.run(G_loss, feed_dict={G_X: generator_x,G_Y:labels,G_is_train:False, G_keep_prob_fc:1.0, D_X: images,D_is_train:False, D_keep_prob_fc:1.0})
                 else:
                     print ('{} is error!', arch_model)
 
@@ -181,7 +194,7 @@ def train_GANs(train_data,train_label,valid_data,valid_label,train_dir,num_class
                 G_epoch_ls, G_samples = sess.run([G_loss, G_net], feed_dict={G_X: generator_x, G_keep_prob_fc:1.0, G_is_train:False})
             elif arch_model == "arch_dcgan_conditional":
                 images, labels = get_next_batch_from_path(valid_data, valid_label, batch_i%(int(valid_n/batch_size)), height, width, batch_size=batch_size, training=False)
-                G_epoch_ls, G_samples = sess.run([G_loss, G_net], feed_dict={G_X: generator_x, G_Y:labels, G_keep_prob_fc:1.0, G_is_train:False})
+                G_epoch_ls, G_samples = sess.run([G_loss, G_net], feed_dict={G_X: generator_x, G_Y:labels, G_keep_prob_fc:1.0, G_is_train:False, D_X: images,D_is_train:False, D_keep_prob_fc:1.0})
 
             G_valid_ls = G_valid_ls + G_epoch_ls
         fig = show_images(G_samples[:16])
